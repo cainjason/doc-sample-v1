00380ef593061cac859763f21da06e2392859b68
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-03-18 00:59:59 -0400

    installed styled components

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,5 +1,30 @@
+import styled from "styled-components";
+
 function Body() {
-  return <div>I'm the Body</div>;
+  return (
+    <GridOptionContainer>
+      <GridOption className="bg-pink-200 h-20 text-white w-full">
+        <label>Option One</label>
+        <image src="" className="" alt=""></image>
+      </GridOption>
+      <GridOption className="bg-blue-200 h-20 text-white w-full">
+        <label>Option Two</label>
+        <image src="" className="" alt=""></image>
+      </GridOption>
+      <GridOption className="bg-green-200 h-20 text-white w-full">
+        <label>Option Three</label>
+        <image src="" className="" alt=""></image>
+      </GridOption>
+      <GridOption className="bg-orange-200 h-20 text-white w-full">
+        <label>Option Four</label>
+        <image src="" className="" alt=""></image>
+      </GridOption>
+    </GridOptionContainer>
+  );
 }
 
 export default Body;
+
+const GridOptionContainer = styled.div``;
+
+const GridOption = styled.div``;
